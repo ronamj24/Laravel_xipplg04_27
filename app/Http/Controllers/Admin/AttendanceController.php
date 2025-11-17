14f05c24819ed3c3446cccda3202d9087aae77b0
ronamj24 <ronamiftahulj@gmail.com>
2025-11-17 12:57:41 +0700

    Add Feature Delete Attendance

--- app/Http/Controllers/Admin/AttendanceController.php
+++ app/Http/Controllers/Admin/AttendanceController.php
@@ -68,6 +68,8 @@ class AttendanceController extends Controller
      */
     public function destroy(string $id)
     {
-        //
+         $attendance = Attendance::findOrFail($id);
+    $attendance->delete();
+    return redirect()->route('admin.attendances.index');
     }
 }
